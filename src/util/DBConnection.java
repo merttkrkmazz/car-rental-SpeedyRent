59d23f64a70f1a017ad32d034433bf54d37a8b83
mardaguler <arda.guler@ozu.edu.tr>
2025-04-30 16:13:39 +0300

    carInfo ne

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -32,10 +32,76 @@ public class Srent_DB {
 
     }
 
-    public static boolean authenticateUser(int userID, String password) {}
-    public static boolean reserveCar(int userID, int carID){}
-    public static boolean cancelReservation(int bookingID){}
-    public static boolean addCar(Object carInfo){}
+    /* checks if the user is exist or not */
+    public static boolean authenticateUser(int userID, String password) {
+        try {
+            boolean isFound;
+            conn = getConnection();
+            String sql;
+
+            sql = "SELECT * FROM user WHERE user_id = ? AND password = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, userID);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            conn.close();
+
+            isFound = rs.next();
+            System.out.println(isFound ? "User is found" : "User is not found");
+
+            return isFound;
+
+        } catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("Error occured! \n");
+        }
+
+    }
+
+    /* reserves car for that user with the car user selected and if the enaough amount
+    *  if he/she MADE booking, so reserve that booked ones, available olmalı, amount üstü olmalı,
+    *  booking (confirmed) olmalı
+    */
+    public static boolean reserveCar(int userID, int carID){
+        try {
+            conn = getConnection();
+            String sql;
+
+            sql = "INSERT INTO reserves VALUES(?,?) ";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, userID);
+            ps.setInt(2, carID);
+            ResultSet rs = ps.executeQuery();
+            conn.close();
+
+            return rs.next();
+
+        } catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("Error occured! \n");
+        }
+    }
+    public static boolean cancelReservation(int bookingID){
+        try {
+            conn = getConnection();
+            String sql;
+
+            sql = "DELETE FROM reservations WHERE booking_id = ? ";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, bookingID);
+            ResultSet rs = ps.executeQuery();
+            conn.close();
+
+            return rs.next();
+
+        } catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("Error occured! \n");
+        }
+    }
+    public static boolean addCar(Object carInfo){
+        /******** what is car info **********/
+    }
     public static boolean updateCar(int carID, Object newInfo){}
     public static boolean deleteCar(int carID){}
     public static boolean checkAvailability(int carID){}

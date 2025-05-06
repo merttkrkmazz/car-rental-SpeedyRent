3687ce7e0832e9d9cd68c1c63ca4e50702c7dc10
Orkun00 <orkun.acar@ozu.edu.tr>
2025-05-06 06:26:15 +0300

    Added DBConnection.java and implemented mock setup for db.

---
+++ src/db/DBConnection.java
@@ -0,0 +1,20 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/s_rent";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+}
